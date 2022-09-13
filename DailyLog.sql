b368c05871359d0643f766f88baf79c244669f97
idunnoguesswho <51185866+idunnoguesswho@users.noreply.github.com>
2022-09-13 16:51:45 -0600

    Create DailyLog.sql
    Initial

---
+++ DailyLog.sql
@@ -0,0 +1,10 @@
+CREATE TABLE DailyLog(
+  Timestamp As DateTime2,
+  EmailAddress as varchar(100),
+  ProjectArea as varchar(100),
+  MeetingType as varchar(50),
+  ReviewTopic as varchar(100),
+  WorkerInput as varchar(255),
+  ActionItems as varchar(255),
+  AreaLead as varchar(255)
+  )
