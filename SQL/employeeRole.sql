4cc673046ae56e8b66cf0e80bf6a693b677107eb
idunnoguesswho <51185866+idunnoguesswho@users.noreply.github.com>
2022-09-19 13:12:58 -0600

    Update employeeRole.sql
    LOA & Expense Allowance

--- SQL/employeeRole.sql
+++ SQL/employeeRole.sql
@@ -1,8 +1,11 @@
 CREATE TABLE employeeRole(
-employeeID int,
-roleName varchar(100),
-projectID int,
-payRate currency,
-StartDate date,
-endDate date
+  employeeID int,
+  roleName varchar(100),
+  projectID int,
+  payRate currency,
+  StartDate date,
+  endDate date,
+  SupervisorID int, --FK: Employee.EmployeeID
+  LOARate currency,
+  ExpensesAllowed bit
 )
